fad1dfbb5619c99def15e1ca22fb2366b12a3bd0
Firomsa Mahamud <MFiromsa@gmail.com>
2025-01-27 23:03:08 -0600

    dynamic rps

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+const reset = document.querySelector('#reset')
+const score = document.querySelector('#score')
+const results = document.querySelector('#results')
+const gameCount = document.querySelector('#gameCount')
+
+let humanScore = 0
+let computerScore = 0
+let game = 0
+
+results.textContent = `User Score: ${humanScore}, Computer Score: ${computerScore}`
+
 // Get computer choice, we need to have it return string values of either "rock", "paper", "scissors"
 // We need computer to choice a random number between 1-3 with Math.random
 // Compare the random number generated with the computer
@@ -18,20 +32,6 @@ function getComputerChoice() {
   }
 }
 
-function correctInput(input) {
-  if (input === 'rock' || input === 'paper' || input === 'scissors') {
-    return input
-  } else {
-    return 'Invalid Entry'
-  }
-}
-
-//Get user input and store it in a variable
-function getHumanChoice() {
-  let user = prompt('Choice rock, paper, or scissors').toLowerCase()
-  return correctInput(user)
-}
-
 // Create a function that takes the computer and human choices as parameters
 // Create conditionals that determines the win of the round
 // all the scenerio that human can win
@@ -40,90 +40,79 @@ function getHumanChoice() {
 // user === 'scissors' && cpu === 'paper'
 // Alert the winner
 
-// function playRound(user, cpu) {
-//   if (user === 'Invalid Entry') {
-//     return console.log('User has entered an Invalid Entry')
-//   }
-
-//   if (
-//     (user === 'rock' && cpu === 'scissors') ||
-//     (user === 'paper' && cpu === 'rock') ||
-//     (user === 'scissors' && cpu === 'paper')
-//   ) {
-//     console.log(`YOU WIN!!!! ${user} beat ${cpu}`)
-//   } else if (
-//     (cpu === 'rock' && user === 'scissors') ||
-//     (cpu === 'paper' && suer === 'rock') ||
-//     (cpu === 'scissors' && user === 'paper')
-//   ) {
-//     console.log(`YOU LOSE!!!! ${cpu} beat ${user}`)
-//   } else {
-//     console.log(`It's a tie..... You both selected ${user}`)
-//   }
-// }
-
-// playRound(humanChoice, computerChoice)
-
-//create a playGame that runs 5 times
-//Keep track of the round winners by incrementing the user & computer winner of each round
-//create variable for score for human and computer
-// While game is less than 5
-// run playRound and keep track of the winner
-//Compare the score and return the winner
-
 function playRound(user, cpu) {
-  if (user === 'Invalid Entry') {
-    return console.log('User has entered an Invalid Entry')
-  }
-
   if (
     (user === 'rock' && cpu === 'scissors') ||
     (user === 'paper' && cpu === 'rock') ||
     (user === 'scissors' && cpu === 'paper')
   ) {
-    console.log(`YOU WIN!!!! ${user} beat ${cpu}`)
+    score.textContent = `YOU WIN!!!! ${user} beat ${cpu}`
 
-    return 'user'
+    return 'win'
   } else if (
     (cpu === 'rock' && user === 'scissors') ||
     (cpu === 'paper' && user === 'rock') ||
     (cpu === 'scissors' && user === 'paper')
   ) {
-    console.log(`YOU LOSE!!!! ${cpu} beat ${user}`)
+    score.textContent = `YOU LOSE!!!! ${cpu} beat ${user}`
 
-    return 'cpu'
+    return 'lose'
   } else {
-    console.log(`It's a tie..... You both selected ${user}`)
+    score.textContent = `It's a tie..... You both selected ${user}`
   }
 }
 
-function playGame() {
-  let humanScore = 0
-  let computerScore = 0
-  let game = 1
-  let round = ''
-  console.log(game)
-  while (game <= 5) {
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-    round = playRound(humanChoice, computerChoice)
-    console.log(round)
-    if (round === 'user') {
-      humanScore += 1
-    } else if (round === 'cpu') {
-      computerScore += 1
+function playGame(user, cpu) {
+  if (game == 5) {
+    resetGame()
+  }
+  let round = playRound(user, cpu)
+  if (round === 'win') {
+    humanScore += 1
+  } else if (round === 'lose') {
+    computerScore += 1
+  }
+
+  results.textContent = `User Score: ${humanScore}, Computer Score: ${computerScore}`
+  game++
+
+  if (game) {
+    gameCount.textContent = `Game: ${game}`
+  }
+  if (game === 5) {
+    if (humanScore > computerScore) {
+      results.textContent = `You won!!! ${humanScore} : ${computerScore}`
+    } else if (computerScore > humanScore) {
+      results.textContent = `You lost!!! ${computerScore} : ${humanScore}`
     }
-    game++
-  }
-
-  if (humanScore > computerScore) {
-    console.log(`You won!!! ${humanScore} : ${computerScore}`)
-  } else if (computerScore > humanScore) {
-    console.log(`You lost!!! ${computerScore} : ${humanScore}`)
   }
 }
 
-// let humanChoice = getHumanChoice()
-// let computerChoice = getComputerChoice()
-// playRound(humanChoice, computerChoice)
-playGame()
+function resetGame() {
+  humanScore = 0
+  computerScore = 0
+  game = 0
+  score.textContent = ''
+  results.textContent = ''
+  results.textContent = `User Score: ${humanScore}, Computer Score: ${computerScore}`
+  gameCount.textContent = `Game: `
+}
+
+rock.addEventListener('click', (e) => {
+  e.preventDefault
+  playGame('rock', getComputerChoice())
+})
+
+paper.addEventListener('click', (e) => {
+  e.preventDefault
+
+  playGame('paper', getComputerChoice())
+})
+
+scissors.addEventListener('click', (e) => {
+  e.preventDefault
+
+  playGame('scissors', getComputerChoice())
+})
+
+reset.addEventListener('click', resetGame)
